3dc283aab932e9a6a10bc6502d1152c9a6326ed4
sean <seanwambua@gmail.com>
2021-12-06 11:25:17 +0300

    Project Backup for feature install

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product():
+    sku = models.CharField(max_length=255)
+    short_description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
